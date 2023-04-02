5e00dae8a445ba46887b33f71fe092310062c830
parkchaewon18 <0101angela@naver.com>
2023-04-03 02:56:01 +0900

    각 테스트케이스의 도립성 확보되도록 리팩토링

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,25 +1,26 @@
 package com.mysite.sbb;
 
-import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
+
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
-
-
-
-    @Modifying
-    @Transactional
-    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
-    void clearAutoIncrement();
 
 
 }
